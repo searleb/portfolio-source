d28918cf3f9b56ca67764ff1f2a512eb4bc4ec56
bill <billsearle@me.com>
2014-08-29 20:46:10 +1000

    another mobile fix attempt. Uses device width as mobile marker

--- source/javascripts/background-class.js
+++ source/javascripts/background-class.js
@@ -1,6 +1,16 @@
-if ($.browser.mobile === true) {
-	console.log('true')
-	$('#background').addClass('mobile-wrapper');
-} else {
-console.log('false');
-};
+// $(document).ready(function() {
+// 	if ($.browser.mobile === true) {
+// 		console.log('false')
+// 		$('#background').addClass('mobile-wrapper');
+// 	} else {
+// 	console.log('true');
+// 	};
+// });
+
+$( document ).ready(function() {      
+    var isMobile = window.matchMedia("only screen and (max-width: 768px)");
+
+    if (isMobile.matches) {
+        $('#background').addClass('mobile-wrapper');
+    }
+ });
