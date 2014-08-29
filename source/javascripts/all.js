3af7f3dc170edba30761d9eb357238d987d87001
bill <billsearle@me.com>
2014-08-29 21:04:06 +1000

    more media queries to work with mobile. 1024px to handle landscape

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,3 @@
 //= require jquery
-//= require mobile-browser-detect
 //= require background-class
 //= require_tree .
--- source/javascripts/background-class.js
+++ source/javascripts/background-class.js
@@ -1,14 +1,5 @@
-// $(document).ready(function() {
-// 	if ($.browser.mobile === true) {
-// 		console.log('false')
-// 		$('#background').addClass('mobile-wrapper');
-// 	} else {
-// 	console.log('true');
-// 	};
-// });
-
 $( document ).ready(function() {      
-    var isMobile = window.matchMedia("only screen and (max-width: 768px)");
+    var isMobile = window.matchMedia("only screen and (max-width: 1024px");
 
     if (isMobile.matches) {
         $('#background').addClass('mobile-wrapper');
