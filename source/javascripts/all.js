a0318ae42015db2bd05eadf0cd5bfeda50fe730f
bill <billsearle@me.com>
2014-09-07 19:28:39 +1000

    fade working on background

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,14 +3,4 @@
 //= require d3
 //= require trianglify.min
 //= require init-trianglify
-//= require count-up
-//= require coffee-buyer
 //= require_tree .
-
-$(document).ready(function(){
-	var height = $(window).height();
-		$('#background').css({
-			'min-height': height
-		});
-	$('body').velocity("fadeIn", { duration: 1500 });
-});
